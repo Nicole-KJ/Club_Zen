266071576eb06939ef49bd272cf7410b793d46ed
FerCha10 <mfchavarria2000@gmail.com>
2023-04-11 18:18:26 -0600

    Actualización conexion con sql

--- Club_Zen/BackEnd/Program.cs
+++ Club_Zen/BackEnd/Program.cs
@@ -1,6 +1,20 @@
+
+using Entities;
+using Entities.Utilities;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+#region ConnectionString
+
+builder.Services.AddDbContext<BD_Club_ZenContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Add services to the container.
+string connString = builder.Configuration.GetConnectionString("DefaultConnection");
+Util.ConnectionString = connString;
+
+
+#endregion
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- Club_Zen/Entities/BD_Club_ZenContext.cs
+++ Club_Zen/Entities/BD_Club_ZenContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Entities.Utilities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
@@ -9,6 +10,8 @@ namespace Entities
     {
         public BD_Club_ZenContext()
         {
+            var optionsBuilder = new DbContextOptionsBuilder<BD_Club_ZenContext>();
+            optionsBuilder.UseSqlServer(Utilities.Util.ConnectionString);
         }
 
         public BD_Club_ZenContext(DbContextOptions<BD_Club_ZenContext> options)
@@ -34,11 +37,8 @@ namespace Entities
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-GA0U25H\\SQLEXPRESS;Database=BD_Club_Zen;Integrated Security=True;Trusted_Connection=True;");
-            }
+            optionsBuilder.UseSqlServer(Util.ConnectionString);
+            base.OnConfiguring(optionsBuilder);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
