4656c6bd011e630df6063a1ed2dd6f60716dff59
FerCha10 <mfchavarria2000@gmail.com>
2023-04-23 13:10:52 -0600

    Actu views Reserva Ranchito y tennis court

--- Club_Zen/FrontEnd/Controllers/ReservacionTennisController.cs
+++ Club_Zen/FrontEnd/Controllers/ReservacionTennisController.cs
@@ -1,20 +1,28 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FrontEnd.Helpers;
+using FrontEnd.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FrontEnd.Controllers
 {
     public class ReservacionTennisController : Controller
     {
+       ReservacionTennisHelper reservacionTennisHelper;
         // GET: ReservacionTennisController
         public ActionResult Index()
         {
-            return View();
+            reservacionTennisHelper = new ReservacionTennisHelper();
+            List<ReservacionTennisViewModel> list = reservacionTennisHelper.GetAll();
+            return View(list);
         }
 
         // GET: ReservacionTennisController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            reservacionTennisHelper = new ReservacionTennisHelper();
+            ReservacionTennisViewModel reservacionevento = reservacionTennisHelper.Get(id);
+
+            return View(reservacionevento);
         }
 
         // GET: ReservacionTennisController/Create
@@ -26,11 +34,14 @@ namespace FrontEnd.Controllers
         // POST: ReservacionTennisController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(ReservacionTennisViewModel reservacionevento)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                reservacionTennisHelper = new ReservacionTennisHelper();
+                reservacionevento = reservacionTennisHelper.Create(reservacionevento);
+
+                return RedirectToAction("Details", new { id = reservacionevento.IdReservacionTennis});
             }
             catch
             {
@@ -41,16 +52,20 @@ namespace FrontEnd.Controllers
         // GET: ReservacionTennisController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            reservacionTennisHelper = new ReservacionTennisHelper();
+            ReservacionTennisViewModel reservacionevento = reservacionTennisHelper.Get(id);
+            return View(reservacionevento);
         }
 
         // POST: ReservacionTennisController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(ReservacionTennisViewModel reservacionevento)
         {
             try
             {
+                reservacionTennisHelper = new ReservacionTennisHelper();
+                reservacionevento = reservacionTennisHelper.Edit(reservacionevento);
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -62,16 +77,21 @@ namespace FrontEnd.Controllers
         // GET: ReservacionTennisController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            reservacionTennisHelper = new ReservacionTennisHelper();
+            ReservacionTennisViewModel reservacionevento = reservacionTennisHelper.Get(id);
+
+            return View(reservacionevento);
         }
 
         // POST: ReservacionTennisController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(ReservacionTennisViewModel reservacionevento)
         {
             try
             {
+                reservacionTennisHelper = new ReservacionTennisHelper();
+                reservacionTennisHelper.Delete(reservacionevento.IdReservacionTennis);
                 return RedirectToAction(nameof(Index));
             }
             catch
